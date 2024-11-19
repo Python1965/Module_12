41f2849ebf58ca552351d1c68bf7c6c365c6b7fe
Python1965 <python65@rambler.ru>
2024-11-19 11:48:03 +0300

    Add files via upload

---
+++ suite_12_3.py
@@ -0,0 +1,31 @@
+# Домашнее задание по теме "Систематизация и пропуск тестов".
+# ***************************************************************************************
+# Задача "Заморозка кейсов":
+# Подготовка:
+# В этом задании используйте те же TestCase, что и в предыдущем: RunnerTest и TournamentTest.
+#
+# Часть 1. TestSuit.
+#   1. Создайте модуль suite_12_3.py для описания объекта TestSuite. Укажите на него переменной
+#      с произвольным названием.
+#   2. Добавьте тесты RunnerTest и TournamentTest в этот TestSuit.
+#   3. Создайте объект класса TextTestRunner, с аргументом verbosity=2.
+#
+# Часть 2. Пропуск тестов.
+#   1. Классы RunnerTest дополнить атрибутом is_frozen = False и TournamentTest атрибутом is_frozen = True.
+#   2. Напишите соответствующий декоратор к каждому методу (кроме @classmethod), который при значении
+#      is_frozen = False будет выполнять тесты, а is_frozen = True - пропускать и выводить сообщение
+#      'Тесты в этом кейсе заморожены'.
+#
+# Таким образом вы сможете контролировать пропуск всех тестов в TestCase изменением всего одного атрибута.
+# Запустите TestSuite и проверьте полученные результаты тестов из обоих TestCase.
+# ****************************************************************************************
+
+import unittest
+import test_12_3
+
+BigTest = unittest.TestSuite()
+BigTest.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.RunnerTest))
+BigTest.addTest(unittest.TestLoader().loadTestsFromTestCase(test_12_3.TournamentTest))
+
+runner= unittest.TextTestRunner(verbosity=2)
+runner.run(BigTest)
